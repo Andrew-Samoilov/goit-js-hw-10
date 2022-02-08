633b59fb2dfa5cb2cf8eb4c45c34953bf664a687
Andrew Samoilov <veprsag@gmail.com>
2022-02-08 12:54:01 +0200

    in progress

--- src/index.js
+++ src/index.js
@@ -3,6 +3,30 @@ import './css/styles.css';
 import './sass/main.scss';
 import { fetchCountries } from './js/fetchCountries';
 
+let dateField= document.querySelector('#search-box');// console.log(dateField);
+
+dateField.addEventListener('input',onFieldInput);
+
+function onFieldInput(){
+    console.log(`dateField.value ${dateField.value}`);
+}
+
 const DEBOUNCE_DELAY = 300;
 
 fetchCountries(22);
+// name.official - полное имя страны
+// capital - столица
+// population - население
+// flags.svg - ссылка на изображение флага
+// languages 
+
+fetch("https://restcountries.com/v3.1/name/ukraine?fields=name,capital,population,flags,languages")
+  .then(response => {
+    // Response handling
+  })
+  .then(data => {
+    // Data handling
+  })
+  .catch(error => {
+    // Error handling
+  });
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,3 +1,5 @@
 export function fetchCountries(name){
     console.log(` inside function fetchCountries `);
+
+
 }
