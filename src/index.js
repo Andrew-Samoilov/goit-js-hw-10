d203e019052818b8aab3d894900f704cfc875db6
Andrew Samoilov <veprsag@gmail.com>
2022-02-08 10:41:00 +0200

    Initialization

---
+++ src/index.js
@@ -0,0 +1,8 @@
+console.log('777');
+import './css/styles.css';
+import './sass/main.scss';
+import { fetchCountries } from './js/fetchCountries';
+
+const DEBOUNCE_DELAY = 300;
+
+fetchCountries(22);
