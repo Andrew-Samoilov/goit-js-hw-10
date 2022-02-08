5b1f0cead86a332823648672613a6842a61be939
Andrew Samoilov <veprsag@gmail.com>
2022-02-08 17:03:45 +0200

    inProgress

--- src/index.js
+++ src/index.js
@@ -1,37 +1,40 @@
-console.log('777');
+console.log('7 77');
 import './css/styles.css';
 import './sass/main.scss';
 import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
 import { fetchCountries } from './js/fetchCountries';
-
-// import getRefs from '.js/get-refs';
+import getRefs from './js/get-refs';
 
 const refs = getRefs();
 
-refs.dateField.addEventListener('input', onFieldInput);
+const DEBOUNCE_DELAY = 300;
+refs.dateField.addEventListener('input', debounce(onFieldInput, DEBOUNCE_DELAY));
 
 function onFieldInput() {
-  let trimedField = dateField.value.trim();
+  let trimedField = refs.dateField.value.trim();
   console.log(`dateField.value ${trimedField}`);
 
   fetchCountries(trimedField)
     .then(renderCountries)
     .catch(onFetchError)
-    .finally(() => console.log(`finalize it`));
-
-  // API.fetchPokemon(searchQuery)
-  // .then(renderPokemonCard)
-  // .catch(onFetchError)
-  // .finally(() => form.reset());
+    .finally(() => Notiflix.Notify.success(`finalize it`));
+    
 }
 
-const DEBOUNCE_DELAY = 300;
-
 function renderCountries(countries) {
-  console.log(`renderCountries ${countries}`);
+  const countriesCount = Object.keys(countries).length;
+  console.log(`Found ${countriesCount} countries`);
+
+  if (countriesCount > 10) {
+    console.log(`Too many matches found. Please enter a more specific name.`);
+    Notiflix.Notify.info(`Too many matches found. (${countriesCount}) Please enter a more specific name.`);
+  }
+  console.log(`renderCountries`, countries[0].name.official);
+
 }
 
 function onFetchError(error) {
-  Notiflix.Notify.info(`Too many matches found. Please enter a more specific name.`);
-  Notiflix.Notify.failure(`❌ Oops, there is no country with that name`);
+  console.error('!!!', error);
+  Notiflix.Notify.failure(`❌ Oops, there is no country with that name ${error}`);
 }
