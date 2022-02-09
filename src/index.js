636397880c839d9e25bf182a7b8de382f2500b59
Andrew Samoilov <veprsag@gmail.com>
2022-02-09 13:53:03 +0200

    done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-console.log('7 77');
+console.log('6666');
 import './css/styles.css';
 import './sass/main.scss';
 import Notiflix from 'notiflix';
@@ -9,56 +9,43 @@ import countryCardTpl from './templates/country-card.hbs';
 import countriesCardTpl from './templates/countries-card.hbs';
 const refs = getRefs();
 
-const DEBOUNCE_DELAY = 1000;
+const DEBOUNCE_DELAY = 300;
 refs.dateField.addEventListener('input', debounce(onFieldInput, DEBOUNCE_DELAY));
 
 function onFieldInput() {
     let trimedField = refs.dateField.value.trim();
-    console.log(`dateField.value ${trimedField}`);
+    // console.log(`dateField.value ${trimedField}`);
 
     fetchCountries(trimedField)
         .then(renderCountries)
         .catch(onFetchError)
-        .finally(() => Notiflix.Notify.success(`finalize it`));
+        .finally(() => Notiflix.Notify.info(`finalize it`));
 }
 
 function renderCountries(countries) {
     const countriesCount = Object.keys(countries).length;
-    console.log(`Found ${countriesCount} countries`);
+    // console.log(`Found ${countriesCount} countries`);
 
     if (countriesCount > 10) {
-        console.log(`Too many matches found. Please enter a more specific name.`);
-        Notiflix.Notify.info(
+        Notiflix.Notify.warning(
             `Too many matches found. (${countriesCount}) Please enter a more specific name.`,
         );
-    }
-
-    // if (countriesCount > 2) {
-    //     const markup = countriesCardTpl(countries);
-    //     refs.countryList.innerHTML = markup;
-    //     refs.countryInfo.remove();
-    // }
-
-    if (countriesCount === 1) {
+    } else if (countriesCount === 1) {
         refs.countryList.innerHTML = '';
         const markup = countryCardTpl(countries);
         refs.countryInfo.innerHTML = markup;
     } else {
         refs.countryInfo.innerHTML = '';
-        const markup =[];
+        const markup = [];
         for (const c of countries) {
-           
             markup.push(countriesCardTpl(c));
-            console.log(`countriesCardTpl(c)`, countriesCardTpl(c));
+            // console.log(`countriesCardTpl(c)`, countriesCardTpl(c));
         }
-        // const markup = countriesCardTpl(countries);
         refs.countryList.innerHTML = markup.join('');
     }
-
-    console.log(`renderCountries`, countries[0].name.official);
 }
 
 function onFetchError(error) {
-    console.error('!!!', error);
-    Notiflix.Notify.failure(`❌ Oops, there is no country with that name ${error}`);
+    // console.error('!!!', error);
+    Notiflix.Notify.failure(`Oops, there is no country with that name ${error}`);
 }
